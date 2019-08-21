6d399cb919064746f36002f45da3323fc35487b9
OtacilioN <osmn@cesar.org.br>
2019-08-21 13:16:15 -0300

    Updates

---
+++ precache-manifest.60f59357260b1147f69ba9dfb00f0e83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16b2077b211df57b52cf092cf178b36",
+    "url": "/computadores-fazem-arte/index.html"
+  },
+  {
+    "revision": "8e5580a98653a27e441b",
+    "url": "/computadores-fazem-arte/static/css/main.04e6f07b.chunk.css"
+  },
+  {
+    "revision": "9943e4a652c672403a55",
+    "url": "/computadores-fazem-arte/static/js/2.1addeff5.chunk.js"
+  },
+  {
+    "revision": "8e5580a98653a27e441b",
+    "url": "/computadores-fazem-arte/static/js/main.5c31ce91.chunk.js"
+  },
+  {
+    "revision": "5085a2ca975f990b9f50",
+    "url": "/computadores-fazem-arte/static/js/runtime~main.3cd245d8.js"
+  }
+]);
